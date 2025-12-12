1c8b7c002e0377f375cf04aef178484efe2fd794
Kevin Ruiz <kevingerardoruiz.2001@gmail.com>
2025-12-12 09:10:00 +0100

    agregando estructura para favoritos

--- assets/js/favorites/favoritesView.js
+++ assets/js/favorites/favoritesView.js
@@ -3,6 +3,7 @@ import { favoriteStore } from "./favoriteStore.js";
 
 document.addEventListener('DOMContentLoaded', () => {
    
+    const productList = document.querySelector('.container-products'); 
     
 
 });
